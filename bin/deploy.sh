88473441f6714b679be01861d38b72d72b2d8bfa
Igor Serebryany <igor47@moomers.org>
2019-12-21 16:22:29 -0800

    make deploy.sh resilient against failure

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,21 +1,32 @@
+#!/usr/bin/bash
+
+# protect ourselves from bash nonsense
+set -o errexit     # fail the script if any command fails
+set -o pipefail    # fail if a pipeline fails
+set -o nounset     # fail if we use an unset variable
+
 export RAZZLE_PUBLIC_DIR=/var/app/current/build/public
 export PORT=8081
 export RAZZLE_AMPLITUDE_KEY=`eb printenv | grep -Eo "RAZZLE_AMPLITUDE_KEY.=.(.*)" | sed -E "s/RAZZLE_AMPLITUDE_KEY.=.//"`
 export RAZZLE_CDN_ROOT=https://d24gftfomm9wsp.cloudfront.net
 
-git branch | grep "* master" > /dev/null
-if [ $? -ne 0 ]
-then
-  echo "Branch is not master, aborting deploy" >&2
+function fail() {
+  echo "${1}" >&2
   exit
-fi
+}
 
-git status | grep "nothing to commit" > /dev/null
-if [ $? -ne 0 ]
-then
-  echo "Branch is dirty, aborting deploy" >&2
-  exit
-fi
+function cleanup() {
+  echo "running cleanup..."
+  # move the real .gitignore back
+  mv .gitignore.bak .gitignore 2>/dev/null || echo "never created gitignore.bak"
+  git reset
+}
+
+trap cleanup EXIT
+
+# make sure we're on a clean master
+git branch | grep "* master" > /dev/null || fail "Branch is not master, aborting deploy"
+git status | grep "nothing to commit" > /dev/null || fail "Branch is dirty, aborting deploy"
 
 # take build out of .gitignore for deploy (long story)
 git checkout .gitignore
@@ -26,8 +37,4 @@ mv .gitignore.deploy .gitignore
 npm run build
 git add build
 aws s3 sync --size-only --acl public-read  build/public/ s3://spaceship-earth/public/
-eb deploy Spaceship-$1 --staged
-
-# move the real .gitignore back
-mv .gitignore.bak .gitignore
-git reset
+eb deploy Spaceship-$1 --staged || echo "either EB failed or you CTRL-Ced it!"
